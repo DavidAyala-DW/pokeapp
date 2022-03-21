9aa03f9c8f7e64a0ca349ee10fce97568e9131c2
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-03-20 21:46:06 -0500

    Favorites page has been replaced

---
+++ pages/favorites/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from '../../components/layouts'
+
+function index() {
+  return (
+    <Layout title={"Favorites"}>
+      <h1>Favorites Page</h1>
+    </Layout>
+  )
+}
+
+export default index
